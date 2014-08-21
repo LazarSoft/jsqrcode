007e8b0b89b4ad685725323c65b2b1e7d99c3ef2
Edgar Hipp <hipp.edg@gmail.com>
2014-08-21 10:41:49 +0200

    upgrade to v0.0.4

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,11 @@ var watch= require('gulp-watch');
 var concat= require('gulp-concat');
 
 gulp.task('watch', function () {
-    path="src/*.js"
+    path=["grid.js", "version.js", "detector.js", "formatinf.js", "errorlevel.js", "bitmat.js", "datablock.js", "bmparser.js", "datamask.js", "rsdecoder.js", "gf256poly.js", "gf256.js", "decoder.js", "qrcode.js", "findpat.js", "alignpat.js", "databr.js","qrcode.node.js"]
+    path.forEach(function(p,i){
+        path[i]="src/"+p;
+    })
+    console.log( path )
 	gulp.src(path)
 		.pipe(watch(function(files) {
             gulp.src(path)
