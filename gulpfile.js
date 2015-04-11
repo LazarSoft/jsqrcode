bee1266a3b9ab75aeabfe80abba9333abaf0cd23
Edgar Hipp <hipp.edg@gmail.com>
2015-04-11 13:58:18 +0200

    added travis.yml for automatic testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,24 @@
 var gulp = require('gulp');
 var watch= require('gulp-watch');
 var concat= require('gulp-concat');
+var path=["grid.js", "version.js", "detector.js", "formatinf.js", "errorlevel.js", "bitmat.js", "datablock.js", "bmparser.js", "datamask.js", "rsdecoder.js", "gf256poly.js", "gf256.js", "decoder.js", "qrcode.js", "findpat.js", "alignpat.js", "databr.js","qrcode.node.js"];
+path.forEach(function(p,i){
+    path[i]="src/"+p;
+});
 
 gulp.task('watch', function () {
-    path=["grid.js", "version.js", "detector.js", "formatinf.js", "errorlevel.js", "bitmat.js", "datablock.js", "bmparser.js", "datamask.js", "rsdecoder.js", "gf256poly.js", "gf256.js", "decoder.js", "qrcode.js", "findpat.js", "alignpat.js", "databr.js","qrcode.node.js"]
-    path.forEach(function(p,i){
-        path[i]="src/"+p;
-    })
-    console.log( path )
 	gulp.src(path)
 		.pipe(watch(function(files) {
             gulp.src(path)
             .pipe(concat('index.js'))
-            .pipe(gulp.dest('.'))
-		}));
+            .pipe(gulp.dest('.'));
+	}));
+});
+
+gulp.task('compile',function(){
+   gulp.src(path)
+     .pipe(concat('index.js'))
+     .pipe(gulp.dest('.'));
 });
 
 gulp.task('default',['watch']);
