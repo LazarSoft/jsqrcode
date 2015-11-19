99ac52f3afd2597019e20ebe8fa241bc732818c2
Edgar HIPP <hipp.edg@gmail.com>
2015-11-19 08:22:55 +0100

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,45 @@
 var gulp = require('gulp');
 var watch= require('gulp-watch');
 var concat= require('gulp-concat');
-var path=["grid.js", "version.js", "detector.js", "formatinf.js", "errorlevel.js", "bitmat.js", "datablock.js", "bmparser.js", "datamask.js", "rsdecoder.js", "gf256poly.js", "gf256.js", "decoder.js", "qrcode.js", "findpat.js", "alignpat.js", "databr.js","qrcode.node.js"];
+
+var path=[
+  "grid.js",
+  "version.js",
+  "detector.js",
+  "formatinf.js",
+  "errorlevel.js",
+  "bitmat.js",
+  "datablock.js",
+  "bmparser.js",
+  "datamask.js",
+  "rsdecoder.js",
+  "gf256poly.js",
+  "gf256.js",
+  "decoder.js",
+  "qrcode.js",
+  "findpat.js",
+  "alignpat.js",
+  "databr.js",
+  "qrcode.node.js"
+];
+
 path.forEach(function(p,i){
-    path[i]="src/"+p;
+  path[i]="src/"+p;
 });
 
 gulp.task('watch', function () {
-	gulp.src(path)
-		.pipe(watch(function(files) {
-            gulp.src(path)
-            .pipe(concat('index.js'))
-            .pipe(gulp.dest('.'));
-	}));
+  gulp.src(path)
+  .pipe(watch(function(files) {
+    gulp.src(path)
+    .pipe(concat('index.js'))
+    .pipe(gulp.dest('.'));
+  }));
 });
 
 gulp.task('compile',function(){
-   gulp.src(path)
-     .pipe(concat('index.js'))
-     .pipe(gulp.dest('.'));
+  gulp.src(path)
+  .pipe(concat('index.js'))
+  .pipe(gulp.dest('.'));
 });
 
 gulp.task('default',['watch']);
