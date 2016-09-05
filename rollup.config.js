8dc7c097d352540ab67abcff9c833362c2a975df
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-09-05 22:56:00 +0200

    Use "rollup" for bundling

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+let pkg = require('./package.json');
+
+export default {
+  entry: 'src/index.js',
+  targets: [
+    {
+      dest: pkg['main'],
+      format: 'umd',
+      moduleName: 'QrCode',
+      sourceMap: true
+    }
+  ]
+};
