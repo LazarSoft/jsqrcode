a3adcce6378048686251c4ed5eafd87423314df3
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-09-05 18:33:11 +0200

    Add ESLint code checking support

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    'browser': true,
+    'commonjs': true,
+    'node': true,
+  },
+  extends: 'eslint:recommended',
+  rules: {
+  	'comma-dangle': 'off',
+    'no-console': 'off',
+    'no-constant-condition': 'off',
+    'no-mixed-spaces-and-tabs': 'off',
+    'no-redeclare': 'off',
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+  },
+};
