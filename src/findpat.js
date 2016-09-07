875f28ad69879352d37fe6f4a59a72bf372f8650
Edgar HIPP <hipp.edg@gmail.com>
2016-09-07 12:57:15 +0200

    Backport https://github.com/LazarSoft/jsqrcode/commit/9c47e149327eb369597ed9fc43dbc719cf8641a5

--- src/findpat.js
+++ src/findpat.js
@@ -371,11 +371,10 @@ function FinderPatternFinder() {
 
       var stdDev = Math.sqrt(square / startSize - average * average);
       var limit = Math.max(0.2 * average, stdDev);
-      for (var i = 0; i < this.possibleCenters.length && this.possibleCenters.length > 3; i++) {
+      for (var i = this.possibleCenters.length - 1; i >= 0; i--) {
         var pattern =  this.possibleCenters[i];
         if (Math.abs(pattern.EstimatedModuleSize - average) > limit) {
           this.possibleCenters.splice(i, 1);
-          i--;
         }
       }
     }
