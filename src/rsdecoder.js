dcf9e97b3d77e14e443acf1f193f807bb92fd549
fantaclaus <fantaclaus@users.noreply.github.com>
2017-03-13 07:23:06 +0300

    incorrect Array initialization

--- src/rsdecoder.js
+++ src/rsdecoder.js
@@ -132,7 +132,7 @@ function ReedSolomonDecoder(field)
 			if (numErrors == 1)
 			{
 				// shortcut
-				return new Array(errorLocator.getCoefficient(1));
+				return [ errorLocator.getCoefficient(1) ];
 			}
 			var result = new Array(numErrors);
 			var e = 0;
